e9e7e283c2868638e517287b7ee549ebf6f3ca7f
菜小顺 <3038523973@qq.com>
2024-04-24 15:39:59 +0800

    将第二页完善完毕

--- EduAnalytics/src/views/CourseOverview.vue
+++ EduAnalytics/src/views/CourseOverview.vue
@@ -33,75 +33,61 @@ const configs = [
 
 <template>
   <Header :title="'课程情况'"></Header>
-<div class="course-over-view">
-  <div class="left-item">
+  <div class="course-over-view">
+    <div class="left-item">
+      <dv-border-box8 :dur="5" style="padding-left: 5px;padding-right: 5px">
+      <div style="width: 100%;height: 420px;">
+        <!--    标题        -->
+        <div style="display: flex;justify-content: space-between;margin-bottom: 20px">
+          <h2 style="color: #32C5FF;font-size: 30px;margin-left: 20px">素养评价</h2>
+          <div style="color: greenyellow;font-size: 12px;margin-top: 10px">截止时间:{{ getCurrentDate() }}</div>
+        </div>
+        <!--    一排四个水位图        -->
+        <div
+            style="display: flex;color: white;justify-content: space-around;margin-bottom: 20px;font-weight: 700;font-size: 18px">
+          <dv-decoration-11 style="width:150px;height:50px;">
+            <div >知识与认知</div>
+          </dv-decoration-11>
+          <dv-decoration-11 style="width:150px;height:50px;">
+            <div >道德与情感</div>
+          </dv-decoration-11>
+          <dv-decoration-11 style="width:150px;height:50px;">
+            <div >实践与操作</div>
+          </dv-decoration-11>
+          <dv-decoration-11 style="width:150px;height:50px;">
+            <div >创新与批判</div>
+          </dv-decoration-11>
+        </div>
+        <div style="display: flex;justify-content: space-around">
+          <dv-water-level-pond v-for="config in configs" :key="config" :config="config"
+                               style="width:110px;height:190px"/>
+        </div>
+      </div>
+        <div style="display: flex;width: 100%;justify-content: space-around">
+          <water-level-chart documentId="water-level-chart1" title="人文素养" :water-level="10"></water-level-chart>
+          <water-level-chart documentId="water-level-chart2" title="团队协作能力" :water-level="20"></water-level-chart>
+          <water-level-chart documentId="water-level-chart3" title="实验素养" :water-level="15"></water-level-chart>
+          <water-level-chart documentId="water-level-chart4" title="科学素养" :water-level="15"></water-level-chart>
+          <water-level-chart documentId="water-level-chart5" title="信息化素养" :water-level="15"></water-level-chart>
+          <water-level-chart documentId="water-level-chart6" title="创新素养" :water-level="20"></water-level-chart>
+        </div>
+      <div style="width: 100%;height: 280px">
+        <radar-chart></radar-chart>
+      </div>
+      </dv-border-box8>
+    </div>
+    <div class="right-item">
+      <dv-border-box8 :dur="5">
+        <div style="width: 100%;height: 800px">
+          <scores-line-graph></scores-line-graph>
+        </div>
+        <div style="text-align: center;display: flex;justify-content: flex-end;height: 40px">
+          <dv-button @click="console.log('click')" border="Border6" color="#4bbbb2" style="width: 200px;font-size: 18px;margin-top: 5px;margin-right: 50px">点击前往结果分析</dv-button>
+        </div>
 
-    <div style="width: 100%;height: 450px;margin-top:50px;">
-      <!--    标题        -->
-      <div style="display: flex;justify-content: space-between;margin-bottom: 20px">
-        <h2 style="color: #32C5FF;font-size: 30px;margin-left: 20px">素养评价</h2>
-        <div style="color: greenyellow;font-size: 12px;margin-top: 10px">截止时间:{{ getCurrentDate() }}</div>
-      </div>
-      <!--    一排四个水位图        -->
-      <div
-          style="display: flex;color: white;justify-content: space-around;margin-bottom: 20px;font-weight: 700;font-size: 18px">
-        <dv-decoration-11 style="width:150px;height:50px;">
-          <div >知识与认知</div>
-        </dv-decoration-11>
-        <dv-decoration-11 style="width:150px;height:50px;">
-          <div >道德与情感</div>
-        </dv-decoration-11>
-        <dv-decoration-11 style="width:150px;height:50px;">
-          <div >实践与操作</div>
-        </dv-decoration-11>
-        <dv-decoration-11 style="width:150px;height:50px;">
-          <div >创新与批判</div>
-        </dv-decoration-11>
-<!--        <div >知识与认知</div>-->
-<!--        <div>道德与情感</div>-->
-<!--        <div>实践与操作</div>-->
-<!--        <div>创新与批判</div>-->
-      </div>
-      <div style="display: flex;justify-content: space-around">
-        <dv-water-level-pond v-for="config in configs" :key="config" :config="config"
-                             style="width:110px;height:190px"/>
-      </div>
-    </div>
-    <div style="width: 100%;height: 350px">
-      <radar-chart></radar-chart>
-    </div>
-
-  </div>
-  <div class="middle-item">
-    <div class="water-level-chart-item">
-      <water-level-chart documentId="water-level-chart1" title="人文素养" :water-level="10"></water-level-chart>
-    </div>
-    <div class="water-level-chart-item">
-      <water-level-chart documentId="water-level-chart2" title="团队协作能力"
-                         :water-level="20"></water-level-chart>
-    </div>
-    <div class="water-level-chart-item">
-      <water-level-chart documentId="water-level-chart3" title="实验素养" :water-level="20"></water-level-chart>
-    </div>
-    <div class="water-level-chart-item">
-      <water-level-chart documentId="water-level-chart4" title="科学素养" :water-level="15"></water-level-chart>
-    </div>
-    <div class="water-level-chart-item">
-      <water-level-chart documentId="water-level-chart5" title="信息化素养"
-                         :water-level="15"></water-level-chart>
-    </div>
-    <div class="water-level-chart-item">
-      <water-level-chart documentId="water-level-chart6" title="创新素养" :water-level="20"></water-level-chart>
+      </dv-border-box8>
     </div>
   </div>
-  <div class="right-item">
-    <scores-line-graph></scores-line-graph>
-    <div style="text-align: center;display: flex;justify-content: flex-end;">
-      <dv-button @click="console.log('click')" border="Border6" color="#4bbbb2" style="width: 200px;">点击前往结果分析</dv-button>
-    </div>
-
-  </div>
-</div>
 </template>
 
 <style scoped lang="scss">
@@ -109,22 +95,10 @@ const configs = [
   display: flex;
   height: 100%;
   width: 100%;
+  justify-content: space-between;
   .left-item{
-    padding-left: 20px;
-    padding-right: 20px;
-  }
-  .middle-item{
     height: 100%;
-    width: 170px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    margin-top: 20px;
-    .water-level-chart-item {
-      width: 140px;
-      height: 140px;
-    }
-
+    width: 100%;
   }
   .right-item{
     height: 100%;
